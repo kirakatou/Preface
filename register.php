4a0d756016c9dca269fc5ab8d4299474683dc18a
Sutandi Tan <Sutandi Tan>
2016-11-01 19:59:26 +0700

    uncomment header

--- register.php
+++ register.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION["user_id"])){
             if ($result) {
                 $_SESSION['profile_id'] = $id;
                 $_SESSION['post_count'] = 0;
-                // header("Location:home.php");
+                header("Location:home.php");
             }
             else {
                 $query = $conn->prepare("delete from profile where id=?");
